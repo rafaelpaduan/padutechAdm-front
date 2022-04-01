6a9995c027319f110dd0b2f55024726763b11bf4
Rafael Paduan <rafaelpaduan2013@gmail.com>
2022-04-01 00:38:56 -0300

    clients views

---
+++ padutechAdm-front/components/clients/clients-table.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="col-12">
+    <div class="card">
+        <table class="table table-vcenter table-mobile-md card-table">
+            <thead>
+            <tr>
+                <th>Name</th>
+                <!-- <th>Title</th> -->
+                <th>Role</th>
+                <th class="w-1"></th>
+            </tr>
+            </thead>
+            <tbody>
+                <clients-table-row v-for="client in clients" :key="client.id" :client="client" />
+            </tbody>
+        </table>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            users: null
+        };
+    },
+
+    props: ['clients'],
+
+    beforeMount() {
+        this.users = this.$users;
+    }
+}
+</script>
---
+++ padutechAdm-front/pages/clients/create/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container-xl">
+        <div class="row" v-if="!error && ready">
+            <div class="col-12">
+                <p>Teste</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            error: false,
+            ready: false
+        }
+    },
+
+    async mounted() {
+        try {
+            this.ready = true
+        } catch (error) {
+            this.error = true
+            this.ready = true
+
+            console.log(error)
+        }
+    },
+}
+</script>
