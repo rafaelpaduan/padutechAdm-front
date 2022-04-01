94b755356ec0e7d87152f6b4635d9560ea616a24
Rafael Paduan <rafaelpaduan2013@gmail.com>
2022-04-01 00:37:40 -0300

    renamed components

--- padutechAdm-front/components/users/users-table-row.vue
+++ padutechAdm-front/components/users/users-table-row.vue
@@ -25,7 +25,7 @@
         <a href="#" class="" data-bs-toggle="modal" :data-bs-target="'#dropUserModal' + user.id">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="4" y1="7" x2="20" y2="7"></line><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path></svg>
         </a>
-        <modal-danger :modalId="'dropUserModal' + user.id" :user="user" ></modal-danger>
+        <users-modal-danger :modalId="'dropUserModal' + user.id" :user="user" ></users-modal-danger>
         <!-- <div class="dropdown">
         <button class="btn dropdown-toggle align-text-top" data-bs-toggle="dropdown">
             Actions
