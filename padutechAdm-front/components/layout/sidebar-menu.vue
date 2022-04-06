f5d2a4d504f602652b1638461f4a5a66c9291924
Rafael Paduan <rafaelpaduan2013@gmail.com>
2022-04-06 00:08:06 -0300

    removed franchises view

--- padutechAdm-front/components/layout/sidebar-menu.vue
+++ padutechAdm-front/components/layout/sidebar-menu.vue
@@ -31,16 +31,6 @@
             </NuxtLink>
         </li>
         <li class="">
-            <NuxtLink class="nav-link link-dark" to="/franchises/">
-                <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-building-skyscraper" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="3" y1="21" x2="21" y2="21"></line><path d="M5 21v-14l8 -4v18"></path><path d="M19 21v-10l-6 -4"></path><line x1="9" y1="9" x2="9" y2="9.01"></line><line x1="9" y1="12" x2="9" y2="12.01"></line><line x1="9" y1="15" x2="9" y2="15.01"></line><line x1="9" y1="18" x2="9" y2="18.01"></line></svg>
-                </span>
-                <span class="nav-link-title">
-                    Franquias
-                </span>
-            </NuxtLink>
-        </li>
-        <li class="">
             <NuxtLink class="nav-link link-dark" to="/tickets/">
                 <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-ticket" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="15" y1="5" x2="15" y2="7"></line><line x1="15" y1="11" x2="15" y2="13"></line><line x1="15" y1="17" x2="15" y2="19"></line><path d="M5 5h14a2 2 0 0 1 2 2v3a2 2 0 0 0 0 4v3a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-3a2 2 0 0 0 0 -4v-3a2 2 0 0 1 2 -2"></path></svg>
--- padutechAdm-front/pages/franchises/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <p>Franchises</p>        
-</template>
